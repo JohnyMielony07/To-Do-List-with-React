5f2de3bea27ba8e8445cf3a1ef60f7f881643d89
JohnyMielony07 <jan.latoch@gmail.com>
2019-01-05 12:40:37 +0100

    add TaskComponent.js

---
+++ todo-list/src/containers/TaskComponent/TaskComponent.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+import Aux from '../../hoc/Ax';
+
+class TaskComponent extends Component {
+
+    render() {
+        return (
+            <Aux>
+                <p>It's a very nice app to store your task using database. Enjoy!</p>
+            </Aux>
+        );
+    }
+}
+
+export default TaskComponent;
